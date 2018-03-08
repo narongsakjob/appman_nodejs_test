0e374b219d96cf97d4855b25a5fd9d45b1457e22
Narongsak <narongsak.chobsri@gmail.com>
2018-03-08 20:39:51 +0700

    Add function dataUsage but not finish

--- problems/problem_06/data-usage.js
+++ problems/problem_06/data-usage.js
@@ -5,6 +5,21 @@ const raw = readFileSync(logFilePath, 'utf8');
 
 const dataUsage = () => {
   // put your code here !!
+  let line = raw.match(/[^\r\n]+/g);
+  let data = []
+
+  line.forEach((word) => {
+    let temp = word.split(',')
+    data.push({
+      date: temp[0],
+      user: temp[1].split('=')[1],
+      data: temp[2].split('=')[1]
+    })
+  })
+
+  console.log(data)
 };
+
+dataUsage()
 
 module.exports = { dataUsage };
