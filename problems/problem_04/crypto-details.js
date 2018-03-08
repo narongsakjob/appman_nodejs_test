1cda6c6b057b093fc8d46c9a4b5b5d30a4eff40f
Narongsak <narongsak.chobsri@gmail.com>
2018-03-08 16:08:29 +0700

    Add function cryptoDetails

--- problems/problem_04/crypto-details.js
+++ problems/problem_04/crypto-details.js
@@ -1,5 +1,19 @@
-const cryptoDetails = async (symbols = []) => {
-  // put your code here !!
-};
+const axios = require('axios')
 
-module.exports = { cryptoDetails };
+const cryptoDetails = async (symbols = []) => {
+  let tickers = []
+  axios({
+    method: 'get',
+    url: 'https://coinbin.org/coins',
+  }).then((res) => {
+    const coins = Object.values(res.data.coins);
+    symbols.forEach(symbol => {
+      tickers.push(coins.filter(data => data.ticker === symbol))
+    })
+    console.log(tickers);
+  })
+  return tickers
+};
+cryptoDetails(['eth', 'xrp'])
+
+module.exports = { cryptoDetails }
