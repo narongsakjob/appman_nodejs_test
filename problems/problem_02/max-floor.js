32d6b3701725a21ee341a62e678337c8440437e1
Narongsak <narongsak.chobsri@gmail.com>
2018-03-08 18:28:16 +0700

    Add function maxFloor

--- problems/problem_02/max-floor.js
+++ problems/problem_02/max-floor.js
@@ -1,5 +1,12 @@
 const maxFloor = input => {
   // put your code here !!
+  let i = 1;
+  for(i; i < input-i; i++) {
+    input -= i;
+  }
+  return i
 };
+
+console.log(maxFloor(5))
 
 module.exports = { maxFloor };
